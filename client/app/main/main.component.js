a35c82400f3b682e7d152d852650602511ed0670
Renganathan <renganathan.1998@gmail.com>
2017-06-22 23:22:59 +0530

    Changed Homepage

--- client/app/main/main.component.js
+++ client/app/main/main.component.js
@@ -8,31 +8,66 @@ export class MainController {
 	
 	constructor(){
 
-		var old_deg=0, deg=0;
+		var old_deg=0, deg=0, deg2 = 0,x=0;
+		var vis0 = 0, vis1 = 1, vis2 = 2, vis3 = 3, vis4 = 4;
+
+		if( ($(window).innerWidth()) < 900)
+	    {
+	    	var scale = $(window).innerWidth()/1200;
+	    	$(".box-content").css("transform","scale(" + scale + ")" );
+	    }
+	    else
+	    	$(".box-content").css("transform","scale(1)" );
+
+	    $(window).on('resize',function(){
+	      
+	     	if( ($(window).innerWidth()) < 900)
+	      	{
+	      		var scale = $(window).innerWidth()/1200;
+	      		$(".box-content").css("transform","scale(" + scale + ")" );
+	      	}
+	      	else
+	    		$(".box-content").css("transform","scale(1)" );
+
+	    })
 		
 		$("#back").on('mousewheel  DOMMouseScroll',function(e){
 
 		    e.stopPropagation();
 		    e.preventDefault();
-		    
 		    var scroll = e.wheelDelta || -e.detail || e.originalEvent.wheelDelta || -e.originalEvent.detail;
 		    var dir;
 
-		    if(scroll<0){
+		    if(scroll<0)
 		    	dir = -1;
-		    }
-		    else{
+		    else
 		    	dir = 1;
-		    }
 
-		    deg = dir*80;
-		    
-		    deg = deg + old_deg;
-		    
+		    deg = dir*72;
+		    deg = deg + old_deg;		    
 		   	old_deg = deg;
 
-		    $("#rotator").css( "transform","rotateY(" + deg + "deg)" );
-		
+		    $("#revolver").css( "transform","rotateY(" + deg + "deg)" );
+		    
+	    	if(deg%144==0)
+	    		deg2 = 360;
+	    	else
+	    		deg2 = 0;
+
+	    	$("#_007").css( "transform","rotateZ(" + deg2 +"deg)" );
+	    	$("#man").css( "transform","rotateY(" + deg2 +"deg)" );
+	    	
+	    	if(deg2==0)
+	    	{
+	    		$("#dance").css( "opacity","0" );
+	    		$("#ironman").css( "transform","scale(0.2) rotateZ(360deg)" );
+	    	} 
+	    	else
+	    	{
+	    		$("#dance").css( "opacity","1" );
+	    		$("#ironman").css( "transform","scale(1) rotateZ(0deg)" );
+	    	}
+
 		})
 	}
 }
