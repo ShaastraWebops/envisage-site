e308540f9e5f89a4c319ea6017d154d5aaddc9c5
Gokulan <gokulan97@gmail.com>
2017-07-30 16:54:14 +0530

    signup working

--- client/app/account/signup/signup.controller.js
+++ client/app/account/signup/signup.controller.js
@@ -13,7 +13,7 @@ export default class SignupController {
       name: '',
       email: '',
       password: '',
-      linkedin: this.linkedin
+      linkedin: ''
     };
     this.errors = {};
     this.submitted = false;
@@ -22,7 +22,7 @@ export default class SignupController {
 
   register(form) {
     this.submitted = true;
-
+    console.log('comes here');
     if(form.$valid) {
       return this.Auth.createUser({
         name: this.user.name,
