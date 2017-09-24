807e41b2229360d153d5d05d550967471261b905
avinashselvam <avinashpanneer@gmail.com>
2017-09-24 14:55:21 +0530

    route added

---
+++ client/app/projectview/projectview.component.js
@@ -0,0 +1,22 @@
+'use strict';
+const angular = require('angular');
+
+const uiRouter = require('angular-ui-router');
+
+import routes from './projectview.routes';
+
+export class ProjectviewComponent {
+  /*@ngInject*/
+  constructor() {
+    this.message = 'Hello';
+  }
+}
+
+export default angular.module('projectview', [uiRouter])
+  .config(routes)
+  .component('projectview', {
+    template: require('./projectview.html'),
+    controller: ProjectviewComponent,
+    controllerAs: 'projectviewCtrl'
+  })
+  .name;
---
+++ client/app/projectview/projectview.component.spec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+describe('Component: ProjectviewComponent', function() {
+  // load the controller's module
+  beforeEach(module('projectview'));
+
+  var ProjectviewComponent;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function($componentController) {
+    ProjectviewComponent = $componentController('projectview', {});
+  }));
+
+  it('should ...', function() {
+    expect(1).to.equal(1);
+  });
+});
